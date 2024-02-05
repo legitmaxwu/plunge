2a2392ac96ac894235a8e4c46083b6d4a424f54a
Max Wu <chengmaxwu@gmail.com>
2024-02-04 23:19:03 -0800

    Fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,13 @@ import "dotenv/config";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  connectionString: `mysql://${process.env.DATABASE_USERNAME ?? ""}:${
-    process.env.DATABASE_PASSWORD ?? ""
-  }@${process.env.DATABASE_HOST ?? ""}/${
-    process.env.DATABASE_NAME ?? ""
-  }?ssl={"rejectUnauthorized":true}`,
+  driver: "mysql2",
+  dbCredentials: {
+    uri: `mysql://${process.env.DATABASE_USERNAME ?? ""}:${
+      process.env.DATABASE_PASSWORD ?? ""
+    }@${process.env.DATABASE_HOST ?? ""}/${
+      process.env.DATABASE_NAME ?? ""
+    }?ssl={"rejectUnauthorized":true}`,
+  },
   tablesFilter: ["plunge_*"],
 } satisfies Config;
